d1bbf882a783292dabf9b5c00bd0259ddbccb2c2
Justin Wang <justinwang24@yahoo.com.au>
2020-08-18 22:49:57 +1000

    * .gitignore: * ResearchAssessmentProgram.sln: * Program.cs:
    * ResearchAssessmentProgram.csproj: 项目初始化

---
+++ ResearchAssessmentProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResearchAssessmentProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
